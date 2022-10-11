0da59d6fef7d396da01e06c49e1e087ec007bb81
Bruno OLMOS OSORIO <bolmos-o@e1r5p7.clusters.42paris.fr>
2022-10-11 20:03:03 +0200

    developped the source for level2; commented level2 walkthrough; added gitignore for binaries; deleted unused files and binaries

--- level2/source.c
+++ level2/source.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 char	*p(void)
 {
-	char buf[80];
+	char buf[64];
+	int n[4];
+	int o;
 
 	fflush(stdout);
 	gets(buf);
 
+	n[0] = o;
+	if ((buf[0] & 0xb0000000) == 0xb0000000 ) {
+		printf("(%p)\n", pointer);
+		_exit(1);
+	}
+
+	puts(buf);
+	strdup(buf);
 }
 
 
 
 int main(void)
 {
-	return p();
+	p();
 }
