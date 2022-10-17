1909c44384320bf06458cff1b4a1aee147a82935
Christie BOUTIER <cboutier@e3r3p9.clusters.42paris.fr>
2022-10-17 11:34:20 +0200

    level7 - added missing library in source.c

--- level7/source.c
+++ level7/source.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <time.h>
+
+char c[68];
 
 void m()
 {
@@ -23,7 +27,6 @@ void m()
 //     // strcpy((char *)ptr1 + 4, argv[1]);
 // }
 
-char c[68];
 
 int main(int argc, char **argv)
 {
@@ -39,8 +42,8 @@ int main(int argc, char **argv)
     ptr2[0] = 2;
     ptr2[1] = malloc(8);
 
-    strcpy(ptr1[1], argv[1]);
-    strcpy(ptr2[1], argv[2]);
+    strcpy((char *)ptr1[1], argv[1]);
+    strcpy((char *)ptr2[1], argv[2]);
 
     file = fopen("/home/user/level8/.pass", "r");
     fgets(c, 68, file);
