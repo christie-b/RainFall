792459f628d1b1bf1a85fd8bb6c33b90bf4aa554
Christie BOUTIER <cboutier@e3r3p9.clusters.42paris.fr>
2022-10-18 18:23:09 +0200

    level9 - modified source.cpp

--- level9/source.cpp
+++ level9/source.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
+#include <cstdio>
 
 class N
 {
 	public:
+
 	N(int n) : number(n)
-	{}
+	{
+	}
 
 	void	setAnnotation(char *str)
 	{
 		memcpy(buf, str, strlen(str));
 	}
 
-	int operator+(N& n)
+	virtual int operator+(N& n)
 	{
-		return number + n->number;
+		return number + n.number;
 	}
 
-	int operator-(N& n)
+	virtual int operator-(N& n)
 	{
-		return number - n->number;
+		return number - n.number;
 	}
 
 	private:
 	int	number;
 	char buf[108];
-}
+};
 
 
 int main(int argc, char **argv)
 {
 	if (argc <= 1)
-		_exit(1);
+		exit(1);
 
 	N *nb1 = new N(5);
 	N *nb2 = new N(6);
 
 	nb1->setAnnotation(argv[1]);
 
-	// something
-	return (?);
+	*nb1 + *nb2;
+	return (0);
 }
