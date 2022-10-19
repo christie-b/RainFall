7795ee84c4d5913f61fa8c9198e4c89d5d43fbc6
Christie BOUTIER <cboutier@e3r3p9.clusters.42paris.fr>
2022-10-19 15:13:15 +0200

    bonus1 - updated source.c

--- bonus1/source.c
+++ bonus1/source.c
@@ -2,5 +2,16 @@
 
 int main (int argc, char **av)
 {
-	
+	int	nb;
+	char buf[40]; //0x3c (esp+60) - 0x14 (esp+20)
+
+	nb = atoi(argv[1]);
+	if (nb > 9)
+		return (1);
+	memcpy(buf, argv[2], nb * 4);
+	if (nb == 1464814662) //0x574f4c46
+	{
+		execl("/bin/sh", "sh", 0);
+	}
+	return (0);
 }
