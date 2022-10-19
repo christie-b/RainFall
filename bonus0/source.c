e91bfc9eaaae7ca969d8574e3d4d6e67323615a1
Christie BOUTIER <cboutier@e3r3p9.clusters.42paris.fr>
2022-10-19 13:10:41 +0200

    bonus0: updated walkthrough and source.c

--- bonus0/source.c
+++ bonus0/source.c
@@ -1,10 +1,44 @@
 
 
 
+void p(char *buf, char *str)
+{
+	char *buf3[4096];
+	char *ret;
 
+	puts(str);
+	read(0, buf3, 4096);
+	ret = strchr(buf3, '\n'); // 10 is \n
+	*ret = 0;
+	strncpy(buf, buf3, 20);
+
+}
+
+void	pp(char *buf)
+{
+	char buf1[20];
+	char buf2[20];
+
+	p(buf1, " - ");
+	p(buf2, " - ");
+
+	strcpy(buf, buf1);
+
+	int i = 0;
+	while (buffer[i])
+	{
+		i++;
+	}
+	buf[i] = ' ';
+	strcat(buf, buf2);
+}
 
 int main(int argc, char **argv)
 {
-	
+	char buf[42]; // 0x40 - 0x16
 
+	pp(buf);
+	puts(buf);
+
+	return (0);
 }
