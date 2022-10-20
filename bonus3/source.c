800a8d1ed7622ef7cd6b47dacdd44de755232991
Bruno OLMOS OSORIO <bolmos-o@e2r4p5.clusters.42paris.fr>
2022-10-20 18:35:21 +0200

    bonus3 is almost identical

--- bonus3/source.c
+++ bonus3/source.c
@@ -3,40 +3,28 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-// #define PATH 
-// #define MODE 
-
 int main(int argc, char **argv)
 {
 	FILE *file = fopen("test", "r");
+	int buf[33] = {0};
 
-	char buf[116]; // 0x9c - 0x18
-
-	// printf("buf[0] : |%c| | strcmp(buf[0], %s) = %d\n", buf[0], argv[1], strcmp(buf, argv[1]));
-
+	// // printf("buf[0] : |%c| | strcmp(buf[0], %s) = %d\n", buf[0], argv[1], strcmp(buf, argv[1]));
 	// FILE *file = fopen("/home/user/end/.pass", "r");
-	for (int i = 0; i < 33; ++i) {
-		buf[i] = 0;
-	}
-	if (file == 0)
+	if (file == 0 || argc != 2)
 		return (-1);
 	
-	if (argc != 2)
-	{
-		return (-1);
-	}
 	fread(buf, 1, 66, file);
 	buf[65] = 0;
-	*(buf + atoi(argv[1])) = 0;
+	((char *)buf)[atoi(argv[1])] = 0;
 	fread(buf + 66, 1, 65, file);
 	fclose(file);
-	if (strcmp(buf, argv[1]) == 0)
+	if (strcmp((char *)buf, argv[1]) == 0)
 	{
 		execl("/bin/sh", "sh", NULL);
 	}
 	else
 	{
-		puts(buf + 65);
+		puts((char *)buf + 65);
 	}
 	return(0);
 }
