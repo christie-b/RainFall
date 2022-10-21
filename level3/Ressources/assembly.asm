95638f92aff0a217977213257d26041755bf4ad6
Christie BOUTIER <cboutier@e3r12p9.clusters.42paris.fr>
2022-10-21 12:14:46 +0200

    level3: formatted walkthrough

--- level3/Ressources/assembly.asm
+++ level3/Ressources/assembly.asm
@@ -17,12 +17,12 @@ Dump of assembler code for function v:
    0x080484e2 <+62>:	   jne    0x8048518 <v+116>		; exits if it is not equal to 64
    0x080484e4 <+64>:	   mov    0x8049880,%eax        	; saves m into eax
    0x080484e9 <+69>:	   mov    %eax,%edx
-   0x080484eb <+71>:	   mov    $0x8048600,%eax           ; "Wait what?!\n"
+   0x080484eb <+71>:	   mov    $0x8048600,%eax			; "Wait what?!\n"
    0x080484f0 <+76>:	   mov    %edx,0xc(%esp)
-   0x080484f4 <+80>:	   movl   $0xc,0x8(%esp)            ; 12
-   0x080484fc <+88>:	   movl   $0x1,0x4(%esp)            ; 1
+   0x080484f4 <+80>:	   movl   $0xc,0x8(%esp)			; 12
+   0x080484fc <+88>:	   movl   $0x1,0x4(%esp)			; 1
    0x08048504 <+96>:	   mov    %eax,(%esp)
-   0x08048507 <+99>:	   call   0x80483b0 <fwrite@plt>    ; fwrite("Wait what?!\n", 1, 12, stdout);
+   0x08048507 <+99>:	   call   0x80483b0 <fwrite@plt>	; fwrite("Wait what?!\n", 1, 12, stdout);
    0x0804850c <+104>:	movl   $0x804860d,(%esp)         	; "/bin/sh"
    0x08048513 <+111>:	call   0x80483c0 <system@plt>    	; system call on /bin/sh
    0x08048518 <+116>:	leave  
